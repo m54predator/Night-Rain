3bd1f053700e9b74c1c16a4887f871a36a34feda
Alex <Kalininshik@mail.ru>
2017-02-12 16:31:38 +0700

    Load of textures was boosted.

--- Texture.cpp
+++ Texture.cpp
@@ -7,37 +7,25 @@ Texture::~Texture()
 
 bool Texture::Set_Texture(const unsigned char *data, size_t w, size_t h)
 {
-	std::vector<unsigned char> image;
-	size_t width, height;
-	image.resize(w * h * 4);
-
-	std::copy(data, data + w * h * 4, image.begin());
-	width = w;
-	height = h;
-
-
 	// Texture size must be power of two for the primitive OpenGL version this is written for. Find next power of two.
-	size_t u2 = 1;
-	while (u2 < width) {
-		u2 *= 2;
-	}
-	size_t v2 = 1;
-	while (v2 < height) {
-		v2 *= 2;
-	}
-	// Ratio for power of two version compared to actual version, to render the non power of two image with proper size.
-	double u3 = static_cast<double>(width) / u2;
-	double v3 = static_cast<double>(height) / v2;
+	//size_t u2 = 1;
+	//while (u2 < w) {
+	//	u2 *= 2;
+	//}
+	//size_t v2 = 1;
+	//while (v2 < h) {
+	//	v2 *= 2;
+	//}
 
-	// Make power of two version of the image.
-	std::vector<unsigned char> image2(u2 * v2 * 4);
-	for (size_t y = 0; y < height; y++) {
-		for (size_t x = 0; x < width; x++) {
-			for (size_t c = 0; c < 4; c++) {
-				image2[4 * u2 * y + 4 * x + c] = image[4 * width * y + 4 * x + c];
-			}
-		}
-	}
+	//// Make power of two version of the image.
+	//std::vector<unsigned char> image2(u2 * v2 * 4);
+	//for (size_t y = 0; y < h; y++) {
+	//	for (size_t x = 0; x < w; x++) {
+	//		for (size_t c = 0; c < 4; c++) {
+	//			image2[4 * u2 * y + 4 * x + c] = data[4 * w * y + 4 * x + c];
+	//		}
+	//	}
+	//}
 
 	glGenTextures(1, &texture_id);
 	glBindTexture(GL_TEXTURE_2D, texture_id);
@@ -46,12 +34,12 @@ bool Texture::Set_Texture(const unsigned char *data, size_t w, size_t h)
 	glTexImage2D(GL_TEXTURE_2D,
 	             0,
 	             4,
-	             static_cast<GLsizei>(height),
-	             static_cast<GLsizei>(width),
+	             static_cast<GLsizei>(h),
+	             static_cast<GLsizei>(w),
 	             0,
 	             GL_RGBA,
 	             GL_UNSIGNED_BYTE,
-	             &image[0]);
+				 data);
 
 	return 0;
 }
